832b8abc1cce9bb7247faaccd22dcfda3b67ba2d
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-06-30 00:00:49 -0400

    import predicates, comparatives, arithmetics

--- src/ArbReals.jl
+++ src/ArbReals.jl
@@ -1,6 +1,10 @@
 module ArbReals
 
-import Base: hash, convert, promote_rule, string, show
+import Base: hash, convert, promote_rule, string, show,
+    isnan, isinf, isfinite, zero, one,
+    isequal, isless, (==), (!=), (<), (<=), (>=), (>),
+    abs, signbit, sign, copysign, flipsign,
+    (+), (-), (*), (/)
 
 include("NemoLibs.jl")  # ensure needed C libraries
 
