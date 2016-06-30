e27aee8e332b917e6b2ab9168b7ffe32de65fb40
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-06-30 10:43:49 -0400

    fix typo

--- src/ArbWrap/support.jl
+++ src/ArbWrap/support.jl
@@ -5,7 +5,7 @@
 
 function copy{P}(x::ArbArb{P})
    z = init(ArbArb{P})
-   ccall(@libarb(arb_set), Void, (Ptr{ArbArb{P}}), Ptr{ArbArb{P}}), &z, &x)
+   ccall(@libarb(arb_set), Void, (Ptr{ArbArb{P}}, Ptr{ArbArb{P}}), &z, &x)
    return z
 end
 deepcopy{P}(x::ArbArb{P}) = copy(x)
