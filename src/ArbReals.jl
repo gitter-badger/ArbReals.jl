6c677cc3723a2bfc72779a1d0faf45e265f0ba92
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-06-29 23:39:24 -0400

    prepare Arb type Mag

--- src/ArbReals.jl
+++ src/ArbReals.jl
@@ -1,6 +1,8 @@
 module ArbReals
 
-include("NemoLibs.jl")  # ensure needed C libraries 
+import Base: hash, convert, promote_rule, string, show
+
+include("NemoLibs.jl")  # ensure needed C libraries
 
 
 end # module ArbReals
---
+++ src/ArbTypes/Mag.jl
@@ -0,0 +1,60 @@
+#=
+   The mag type used by Arb (fredrikj.net/arb/mag.html)
+   see also (https://github.com/thofma/Hecke.jl/blob/master/src/Misc/mag.jl)
+=#
+
+immutable ArbMag
+    exponent::Int
+    mantissa::UInt64
+end
+
+ArbMag{T<:Union{Int64,Int32}}(exponent::Int, mantissa::T) =
+    ArbMag(exponent, mantissa % UInt64)
+
+function releaseMag{T<:ArbMag}(x::T)
+    ccall(@libarb(mag_clear), Void, (Ptr{T}, ), &x)
+end
+
+function init{T<:ArbMag}(::Type{T})
+    z = ArbMag(zero(Int), zero(UInt64))
+    ccall(@libarb(mag_init), Void, (Ptr{T}, ), &z)
+    finalizer(z, releaseMag)
+    z
+end
+
+ArbMag() = init(ArbMag)
+
+# define hash so other things work
+const hash_arbmag_lo = (UInt === UInt64) ? 0x29f934c433d9a758 : 0x2578e2ce
+const hash_0_arbmag_lo = hash(zero(UInt), hash_arbmag_lo)
+if UInt===UInt64
+   hash(z::ArbMag, h::UInt) = hash( reinterpret(UInt64, z.exponent), z.mantissa )
+else
+   hash(z::ArbMag, h::UInt) = hash( reinterpret(UInt32, z.exponent) % UInt64, z.mantissa )
+end
+
+function convert{T<:ArbMag}(::Type{T}, x::Float64)
+    z = ArbMag()
+    ccall(@libarb(mag_set_d), Void, (Ptr{T}, Ptr{Float64}), &z, &x)
+    z
+end
+function convert{T<:ArbMag}(::Type{T}, x::Float32)
+    convert(T, convert(Float64, x))
+end
+
+function convert{T<:ArbMag}(::Type{T}, x::UInt)
+    z = ArbMag()
+    ccall(@libarb(mag_set_ui), Void, (Ptr{T}, Ptr{UInt}), &z, &x)
+    z
+end
+function convert{T<:ArbMag}(::Type{T}, x::Int)
+    if x < 0
+        throw(ErrorException("Arb magnitudes must be nonnegative"))
+    end
+    convert(T, convert(UInt,x))
+end
+
+for T in (:UInt, :Int, :Float32, :Float64)
+    promote_rule(::Type{ArbMag}, ::Type{$T}) = ArbMag
+end
+
