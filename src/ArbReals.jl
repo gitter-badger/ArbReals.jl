d05937ea1917f08b006a801a7da63b6a38eb39b1
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2016-06-29 23:41:34 -0400

    rename Mag.jl ArbMag.jl

--- src/ArbReals.jl
+++ src/ArbReals.jl
@@ -4,5 +4,8 @@ import Base: hash, convert, promote_rule, string, show
 
 include("NemoLibs.jl")  # ensure needed C libraries
 
+include("ArbTypes/ArbMag.jl")
+include("ArbTypes/Arf.jl")
+include("ArbTypes/Arb.jl")
 
 end # module ArbReals
